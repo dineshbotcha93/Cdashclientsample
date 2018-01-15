b7edb10f19c68da064e45902fa603f16266f0ca0
Hemanth Kakarlapudi <hekakarl@Hemanths-MacBook-Pro.local>
2018-01-15 22:09:55 +0100

    ädded user create and update

--- src/app/user-management/UserManagement.module.ts
+++ src/app/user-management/UserManagement.module.ts
@@ -2,29 +2,31 @@ import { NgModule } from '@angular/core';
 import {Routes,RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 import { EmailValidator } from '@angular/forms';
 import { UserManagementService } from './user-management.service';
 import { UserRegisterComponent } from './user-register/user-register.component';
-
-
+import { UserUpdateComponent } from './user-update/user-update.component';
+import { UserCreateComponent } from './user-create/user-create.component';
 export const routes: Routes = [
-  { path:'user-register',component: UserRegisterComponent }
+{ path:'user-register',component: UserRegisterComponent },
+{ path:'user-create/:id',component: UserCreateComponent },
+{ path:'user-update/:id',component: UserUpdateComponent }
 ]
-
 @NgModule({
-  declarations: [
-    UserRegisterComponent,
-  ],
-    providers:[
-    UserManagementService
-  ],
-  imports:[
-    RouterModule.forChild(routes),
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
+declarations: [
+UserRegisterComponent,
+UserUpdateComponent,
+UserCreateComponent,
+],
+providers:[
+UserManagementService
+],
+imports:[
+RouterModule.forChild(routes),
+CommonModule,
+ReactiveFormsModule,
+FormsModule
+],
 })
 export class UserManagementModule {}
