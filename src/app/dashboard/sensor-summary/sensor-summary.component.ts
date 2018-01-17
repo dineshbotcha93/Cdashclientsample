30e853f402064aa13bd52bbc11453c64d7ddbfe7
Hemanth Kakarlapudi <hekakarl@Hemanths-MacBook-Pro.local>
2018-01-18 00:22:34 +0100

    ädded location dropdown

--- src/app/dashboard/sensor-summary/sensor-summary.component.ts
+++ src/app/dashboard/sensor-summary/sensor-summary.component.ts
@@ -1,44 +1,58 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { MapService } from '../../shared/components/map/services/map.service';
 import { MapConstants } from '../../shared/components/map/constants/map.constants';
 import { SensorSummaryService } from './services/sensor-summary.service';
-
 @Component({
-  selector:'app-sensor-summary',
-  templateUrl:'./sensor-summary.component.html',
-  styleUrls: ['./sensor-summary.component.scss'],
-  providers:[MapService,SensorSummaryService]
+selector:'app-sensor-summary',
+templateUrl:'./sensor-summary.component.html',
+styleUrls: ['./sensor-summary.component.scss'],
+providers:[MapService,SensorSummaryService]
 })
-export class SensorSummaryComponent {
-  mapData:Object = null;
-  allSensors:Array<any> = [];
+export class SensorSummaryComponent implements OnInit{
+mapData:Object = null;
+allSensors:Array<any> = [];
   displayTiles:Object = null;
   orderBy: any = 'asc';
   gateway: any = 'all';
+  locationData: any = [];
+  selectLocation:any = [];
+  locationId:any = null;
   private mapStatus = MapConstants.STATUS;
   constructor(private route:ActivatedRoute, private router:Router,private mapService:MapService,private sensorSummaryService:SensorSummaryService){
-    this.route.params.subscribe((params)=>{
-      console.log(params.id);
-      this.sensorSummaryService.getData(params.id).then((e)=>{
-        console.log('GOT E');
-        console.log(e);
-        this.mapData = e;
-        e.Location.Network.Gateway.forEach((gate)=>{
-          gate.Sensor.forEach((sens)=>{
-            this.allSensors.push(sens);
-          });
-        });
-      });
-    });
-
+  this.route.params.subscribe((params)=>{
+  this.locationId=params.id;
+  this.sensorSummaryService.getData(params.id).then((e)=>{
+  this.mapData = e;
+  e.Location.Network.Gateway.forEach((gate)=>{
+  gate.Sensor.forEach((sens)=>{
+  this.allSensors.push(sens);
+  });
+  });
+  });
+  });
   }
-
+  ngOnInit() {
+  this.mapService.getData().subscribe(e=>{
+  for (let location of e.LocationGroup) {
+  location.Location.forEach((loc)=>{
+  let Obj = {
+  name: null,
+  Id: null
+  };
+  
+  Obj.Id=loc.Id;
+  Obj.name=loc.Title;
+  this.locationData.push(Obj);
+  });
+  this.selectLocation = this.locationData[0];
+  }
+  });
+  }
   gotoSummary(){
-    this.router.navigate(['dashboard/sensor-details','I1']);
+  this.router.navigate(['dashboard/sensor-details','I1']);
   }
-
   doCompare(){
-    this.router.navigate(['dashboard/sensor-comparison','I1']);
+  this.router.navigate(['dashboard/sensor-comparison','I1']);
   }
-}
+  }
