13da054a65331b7359c6597cecff2e8818bb8039
Sridhar <sridhar.pendiala@gmail.com>
2018-02-23 22:59:53 -0500

    Business layer Api integration

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -54,7 +54,7 @@ export class DashboardComponent implements AfterViewInit, AfterContentInit {
         this.totalStatuses['lowBattery'].count+= rResult.lowBattery;
       });
       return realResults;
-    }).then((real)=>{
+    }).then((real) => {
       this.mapData = real;
       this.loadedStatuses = true;
       this.forceTranslations();
