0a093884e6c7ff46172c623fd2475b074e91c725
Rami Mikhail <webdev67@gmail.com>
2018-03-15 22:09:08 -0400

    Connected notificationlist service to component.

---
+++ src/app/dashboard/notificationList/notificationList.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { RequesterService } from '../../shared/services/requester.service';
+
+@Injectable()
+export class NotificationListService {
+  constructor(private requesterService: RequesterService){
+
+  }
+  getNotificationList(){
+    return this.requesterService.getExternalRequest('/api/Notification/SentToAccount?AccountID=168&StartIndex=1&Count=100&FromDate=3-11-2018&ToDate=3-12-2018');
+  }
+}
