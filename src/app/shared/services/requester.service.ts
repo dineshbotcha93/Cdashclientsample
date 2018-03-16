2551ca5f34ac737b4604bf1c8d1347081574900e
Pattabhi Ponugupati <pattabhi.pat@gmail.com>
2018-03-16 18:32:06 +0100

    Working version for user creation

--- src/app/shared/services/requester.service.ts
+++ src/app/shared/services/requester.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { SERVICE_CONSTANTS } from '../../shared/constants/service.constants';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import 'rxjs/operator/map';
 import 'rxjs/operator/catch';
-import { MockBackendService } from '../../../mocks/mock.backend.service';
-import { environment } from '../../../environments/environment';
-import { SERVER_URLS } from '../../shared/constants/serverUrl.constants';
+import {MockBackendService} from '../../../mocks/mock.backend.service';
+import {environment} from '../../../environments/environment';
+import {SERVER_URLS} from '../../shared/constants/serverUrl.constants';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class RequesterService {
   data:String[] = [];
-  constructor(private http:Http,private mockBackendService:MockBackendService) {
+  constructor(private http:Http, private httpClient: HttpClient, private mockBackendService:MockBackendService) {
     if(!environment.production){
       this.mockBackendService.start();
     }
@@ -46,6 +46,15 @@ export class RequesterService {
       throw e;
     })
     .toPromise();
+  }
+
+  postExternalRequestWithHeaders(url: string, body: object, headers: HttpHeaders): Promise<any> {
+
+    const requestOptions = {
+      headers: headers
+    };
+
+    return this.httpClient.post(SERVER_URLS.EXTERNAL_SERVER_URL + url, body, requestOptions).toPromise();
   }
 
   putExternalRequest(url:string,body:object):Promise<any>{
