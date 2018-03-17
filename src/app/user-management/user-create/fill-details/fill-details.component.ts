6d393268175f2530d468c44efe8476fc3edceb86
Pattabhi Ponugupati <pattabhi.pat@gmail.com>
2018-03-17 16:08:47 +0100

    Commit for updating existing user. Should work when the issue with API is sorted

--- src/app/user-management/user-create/fill-details/fill-details.component.ts
+++ src/app/user-management/user-create/fill-details/fill-details.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
-import { FillDetailsService } from './fill-details.service';
-import {Router, ActivatedRoute} from '@angular/router';
-import { FormGroup,FormBuilder ,FormControl,Validators , FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {NewUserRegistrationForm , AccountRegistrationForm } from "./fill-details.component.model";
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {FillDetailsService} from './fill-details.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AccountRegistrationForm} from './fill-details.component.model';
 import {AddressFormComponent} from '../../../shared/components/addressForm/addressForm.component';
+import {add} from 'ngx-bootstrap/chronos';
 
 
 @Component({
@@ -24,44 +25,51 @@ export class FillDetailsComponent implements OnInit, AfterViewInit {
   stepOneData = {};
 
   public accountForm: FormGroup;
+  private accountInfo: any;
   postData: object = {};
 
   @ViewChild('addressForm')
   addressForm: AddressFormComponent;
 
-  constructor(private fillDetailsService: FillDetailsService, private router: Router, private fb: FormBuilder, private route: ActivatedRoute){
+  public accountUpdateStatus: any = {
+    error: false,
+    message: ''
+  };
+
+  constructor(private fillDetailsService: FillDetailsService, private router: Router, private fb: FormBuilder, private route: ActivatedRoute) {
     this.selectedStep = 2;
 
     this.stepOneData = route.data.map(d => d.userData);
 
+    this.accountInfo = {};
 
 
-    this.fillDetailsService.getIndustries().subscribe((e)=>{
-      e.forEach((res)=>{
-        res.forEach((r)=>{
-          if(r.Parent == null){
-            this.industries.push({id:r.IndustryClassificationID, name: r.Name});
+    this.fillDetailsService.getIndustries().subscribe((e) => {
+      e.forEach((res) => {
+        res.forEach((r) => {
+          if (r.Parent == null) {
+            this.industries.push({id: r.IndustryClassificationID, name: r.Name});
           } else {
-            this.businessType.push({id:r.Parent, name: r.Name});
+            this.businessType.push({id: r.Parent, name: r.Name});
           }
         });
-      })
-    })
-    this.fillDetailsService.getTimeZones().subscribe((e)=>{
-      e[0].forEach((tZ)=>{
-        this.timeZones.push({id:tZ.TimeZoneID,name:tZ.DisplayName});
+      });
+    });
+    this.fillDetailsService.getTimeZones().subscribe((e) => {
+      e[0].forEach((tZ) => {
+        this.timeZones.push({id: tZ.TimeZoneID, name: tZ.DisplayName});
       });
     });
     this.placeOfPurchase = [
-      {name:'Foodservice Distributor'},
-      {name:'HVAC/R Wholesaler'},
-      {name:'Industrial Catalog'},
-      {name:'Online'},
-      {name:'Other'}
-    ]
+      {name: 'Foodservice Distributor'},
+      {name: 'HVAC/R Wholesaler'},
+      {name: 'Industrial Catalog'},
+      {name: 'Online'},
+      {name: 'Other'}
+    ];
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.accountForm = this.fb.group({
       company_name: new FormControl('', Validators.required),
       industry_type: new FormControl('', Validators.required),
@@ -73,22 +81,22 @@ export class FillDetailsComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    console.log('implementing after view init');
     this.fillDetailsService.fetchExistingUserInfo()
       .then(data => {
         console.log('user info', data.account[0]);
-        const userInfo = data.account[0];
+        this.accountInfo = data.account[0];
         this.accountForm.patchValue({
-          company_name: userInfo.companyName,
-          timeZone: userInfo.timeZoneID
+          company_name: this.accountInfo.companyName,
+          timeZone: this.accountInfo.timeZoneID,
+          placeOfPurchase: this.accountInfo.reselect
         });
         this.addressForm.addressForm.patchValue({
-          country: userInfo.country,
-          street: userInfo.address,
-          housenumber: userInfo.address2,
-          city: userInfo.city,
-          zipcode: userInfo.postalCode,
-          state: userInfo.state,
+          country: this.accountInfo.country,
+          street: this.accountInfo.address,
+          housenumber: this.accountInfo.address2,
+          city: this.accountInfo.city,
+          zipcode: this.accountInfo.postalCode,
+          state: this.accountInfo.state,
         });
       })
       .catch(error => {
@@ -97,48 +105,73 @@ export class FillDetailsComponent implements OnInit, AfterViewInit {
   }
 
 
-  private addFormControl(name: string, formGroup: FormGroup) : void {
+  private addFormControl(name: string, formGroup: FormGroup): void {
     this.accountForm.addControl(name, formGroup);
   }
 
-  industryChanged(id){
-    console.log("selected industry", id);
-    this.businessTypeSelection = this.businessType.filter((res)=>res['id'] == id );
+  industryChanged(id) {
+    console.log('selected industry', id);
+    this.businessTypeSelection = this.businessType.filter((res) => res['id'] == id);
   }
 
-  onPrevious($event){
+  onPrevious($event) {
     console.log($event);
     this.router.navigate(['user-register/user-create/fill-details']);
   }
 
-  onNext($event){
-    console.log("not implemented yet");
+  onNext($event) {
+    console.log('not implemented yet');
   }
 
-  onSubmit(){
-    console.log("form..");
+  onSubmit() {
     console.log(this.accountForm.value);
-   // if(this.accountForm.valid){
+    // if(this.accountForm.valid){
 
-      console.log(this.accountForm.value);
+    console.log(this.accountForm.value);
 
-      // set the token to the header before making the api call
+    // set the token to the header before making the api call
 
     this.prepareAccountRegistrationForm();
-      // submit data to the new account creation api
-      this.createNewUserAccount(this.accountForm);
+    // submit data to the new account creation api
+    // this.createNewUserAccount(this.accountForm);
 
-   /* } else {
+    this.updateExistingUserAccount(this.accountForm, this.addressForm.addressForm);
 
-    }*/
   }
 
   private createNewUserAccount(accountForm: FormGroup) {
 
-    this.fillDetailsService.createNewUserAccount(accountForm.value).then((e)=>{
+    this.fillDetailsService.createNewUserAccount(accountForm.value).then((e) => {
       console.log(e);
-    })
+    });
 
+  }
+
+  private updateExistingUserAccount(accountForm: FormGroup, addressForm: FormGroup) {
+    console.log('update existing user account', accountForm);
+
+    const payloadData = {
+      accountID: this.accountInfo.accountID,
+      timeZone: accountForm.get('timeZone').value,
+      resellerID: this.accountInfo.resellerID,
+      companyName: accountForm.get('company_name').value,
+      address: addressForm.get('street').value,
+      address2: addressForm.get('housenumber').value,
+      city: addressForm.get('city').value,
+      state: addressForm.get('state').value,
+      country: addressForm.get('country').value,
+      postalCode: addressForm.get('zipcode').value
+    };
+
+    this.fillDetailsService.updateExistingUserInfo(payloadData)
+      .then((data) => {
+        console.log('Update Works', data);
+      })
+      .catch((error) => {
+        this.accountUpdateStatus.error = true;
+        this.accountUpdateStatus.message = error.message;
+        console.log('error occured', error);
+      });
   }
 
   private prepareAccountRegistrationForm() {
--- src/app/user-management/user-create/fill-details/fill-details.service.ts
+++ src/app/user-management/user-create/fill-details/fill-details.service.ts
@@ -1,20 +1,21 @@
-import { Injectable } from '@angular/core';
-import { Observable } from "rxjs/Observable";
-import "rxjs/add/observable/of";
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 import * as INDUSTRIES from './jsons/industries.json';
 import * as TIME_ZONES from './jsons/timeZones.json';
-import { RequesterService } from "../../../shared/services/requester.service";
+import {RequesterService} from '../../../shared/services/requester.service';
 
 @Injectable()
-export class FillDetailsService{
+export class FillDetailsService {
 
   constructor(private requesterService: RequesterService) {
 
   }
 
-  getIndustries():any {
+  getIndustries(): any {
     return Observable.of([INDUSTRIES]);
   }
+
   getTimeZones(): any {
     return Observable.of([TIME_ZONES]);
   }
@@ -29,5 +30,9 @@ export class FillDetailsService{
 
   fetchExistingUserInfo() {
     return this.requesterService.getExternalRequest('/api/User/Info');
+  }
+
+  updateExistingUserInfo(accountInfo: any) {
+    return this.requesterService.putExternalRequest('/api/Account', accountInfo);
   }
 }
