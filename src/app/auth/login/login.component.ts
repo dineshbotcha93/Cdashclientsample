25ae5988e2c87693b33ba4b5b300f85851948064
Pattabhi Ponugupati <pattabhi.pat@gmail.com>
2018-05-12 21:44:19 +0200

    Updated login screen with anonymous payments form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,36 +11,50 @@ import { AlertSandbox } from '../../shared/components/alerts/alerts.sandbox';
   selector: 'app-login',
   styleUrls: ['./login.component.scss'],
   templateUrl: './login.component.html',
-  providers:[LoginSandbox,AlertSandbox],
+  providers: [LoginSandbox, AlertSandbox],
 })
 export class LoginComponent implements OnInit {
   private myform: FormGroup;
+  anonymousPayments: FormGroup;
+  invalidInvoice = false;
   constructor(
     public loginSandbox$: LoginSandbox,
     private translate: TranslateService,
     private alertSandbox: AlertSandbox
-  ){
+  ) {
     this.translate.use('en');
   }
 
   ngOnInit(){
     this.myform = new FormGroup({
-      username: new FormControl('',[Validators.required, Validators.minLength(3),Validators.maxLength(50)]),
-      password: new FormControl('',[Validators.required]),
+      username: new FormControl('', [Validators.required, Validators.minLength(3),Validators.maxLength(50)]),
+      password: new FormControl('', [Validators.required]),
+    });
+
+    this.anonymousPayments = new FormGroup( {
+      invoiceId: new FormControl('', [Validators.required])
     });
   }
 
-  onSubmit(){
-    if(this.myform.valid){
+  onSubmit() {
+    if (this.myform.valid) {
       console.log("form is valid");
       console.log(this.loginSandbox$.doLogin(this.myform));
     } else {
       console.log(this.myform);
-      this.alertSandbox.showAlert({data: "Invalid Login",autohide: false});
+      this.alertSandbox.showAlert({data: "Invalid Login", autohide: false});
     }
   }
 
-  catchLanguage(lang){
+  submitAnonymousPayment() {
+    if (this.anonymousPayments.valid) {
+      alert('valid invoice id');
+    } else {
+      this.invalidInvoice = true;
+    }
+  }
+
+  catchLanguage(lang) {
     this.translate.setDefaultLang(lang);
     this.translate.use(lang);
   }
