5cfea46aea349b01201893e9ff301b8c1b076bd9
Rami Mikhail <webdev67@gmail.com>
2018-05-13 21:54:50 -0400

    Added test code stubs.

--- src/app/business/business.component.spec.ts
+++ src/app/business/business.component.spec.ts
@@ -1,15 +1,65 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA, EventEmitter } from '@angular/core';
 import { BusinessComponent } from './business.component';
+import { ComponentsModule } from '../shared/components';
+import { ChartsModule } from 'ng2-charts';
+import { MapService } from '../shared/components/map/services/map.service';
+import { BusinessService } from './services/business.service';
+import { ActivatedRoute, Router,Routes } from '@angular/router';
+import {RouterTestingModule} from "@angular/router/testing";
+import { AuthGuard } from '../shared/services/auth-guard.service';
 
-describe('BusinessComponent', () => {
+
+fdescribe('BusinessComponent', () => {
   let component: BusinessComponent;
   let fixture: ComponentFixture<BusinessComponent>;
 
+  class MockMapService {
+
+  }
+
+  class MockBusinessService {
+    getRealData(x){
+      return Promise.resolve({});
+    }
+  }
+
+  const routes: Routes = [{
+    path: 'business',
+    canActivate: [AuthGuard],
+    children: [
+        {
+            path: '',
+            component: BusinessComponent
+        },
+        {
+            path: ':customer-list/:status',
+            loadChildren: './customer-list/customer-list.module#CustomerListModule'
+        },
+        {
+           path: ':customer-details/:id/:view',
+           loadChildren: './customer-details/customer-details.module#CustomerDetailsModule'
+        }
+    ]
+  }];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BusinessComponent]
+      declarations: [BusinessComponent],
+      imports:[
+        ComponentsModule,
+        ChartsModule,
+        RouterTestingModule.withRoutes(routes),
+      ],
+      schemas:[CUSTOM_ELEMENTS_SCHEMA],
     })
+    .overrideComponent(BusinessComponent, {
+  set: {
+    providers: [
+      {provide: MapService, useClass: MockMapService},
+      {provide: BusinessService, useClass: MockBusinessService}
+    ]
+  }})
       .compileComponents();
   }));
 
