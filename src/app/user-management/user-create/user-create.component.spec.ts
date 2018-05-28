662d43491c1e4197ab9c132c8c63f4a4ecc2ff20
Rami Mikhail <webdev67@gmail.com>
2018-05-27 23:00:45 -0400

    Added working mocks for user-create.component.spec.ts

--- src/app/user-management/user-create/user-create.component.spec.ts
+++ src/app/user-management/user-create/user-create.component.spec.ts
@@ -1,16 +1,59 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgbModule,NgbTooltipConfig, NgbPopoverConfig } from '@ng-bootstrap/ng-bootstrap';
 import { UserCreateComponent } from './user-create.component';
+import {ActivatedRoute, Router} from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import {UserManagementService} from '../user-management.service';
+import {CommonSharedService} from '../../shared/services/common-shared.service';
 
-describe('UserCreateComponent', () => {
+fdescribe('UserCreateComponent', () => {
   let component: UserCreateComponent;
   let fixture: ComponentFixture<UserCreateComponent>;
 
+  class NgbTooltipConfigMock {
+
+  }
+  class NgbPopoverConfigMock {}
+
+  class CommonSharedServiceMock {
+
+  }
+
+  class UserManagementServiceMock {
+
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserCreateComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        NgbModule
+      ],
+      declarations: [ UserCreateComponent ],
+      providers:[{
+        provide: ActivatedRoute, useValue: {
+          queryParams: Observable.of({ id: 'test' })
+        },
+      },{
+        provide: Router, useValue: {
+          queryParams: Observable.of({queryParams:'test'})
+        }
+      }],
+      schemas:[CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+    .overrideComponent(UserCreateComponent, {
+  set: {
+    providers: [
+      {provide: NgbTooltipConfig, useClass: NgbTooltipConfigMock},
+      {provide: UserManagementService, useClass: UserManagementServiceMock},
+      {provide: CommonSharedService, useClass: CommonSharedServiceMock},
+      {provide: NgbPopoverConfig, useClass: NgbPopoverConfigMock}
+    ]
+  }
+  }).compileComponents();
   }));
 
   beforeEach(() => {
